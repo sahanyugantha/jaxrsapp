9a7a5170ed27b6fcd1b65f0d9d3998840c965a95
Sahan Perera <sahanyuganthamail@gmail.com>
2020-05-24 12:03:28 +0530

    Heroku ClearDB configs updated

--- src/main/java/com/sahan/jaxrsapp/configs/Configs.java
+++ src/main/java/com/sahan/jaxrsapp/configs/Configs.java
@@ -7,10 +7,10 @@ public class Configs {
 	
 	public static Connection getDbConnection() throws ClassNotFoundException, SQLException {
 		
-		String connURL = "jdbc:mysql://localhost:3306/cardb";
+		String connURL = "jdbc:mysql://us-cdbr-east-06.cleardb.net:3306/heroku_677cfadb2807d1c";
 		
 		Class.forName("com.mysql.cj.jdbc.Driver");
-		Connection conn = DriverManager.getConnection(connURL, "root", "12345");
+		Connection conn = DriverManager.getConnection(connURL, "b388ac2ebe8984", "0668a5ce");
 		return conn;
 	}
 
